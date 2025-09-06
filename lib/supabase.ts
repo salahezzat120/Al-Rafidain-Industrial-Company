3fa73c795a19dcc4b6283ca2430d1e3805adee22
salah ezzat <121319130+salahezzt120@users.noreply.github.com>
2025-09-06 18:16:32 +0300

    update supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://uhdxceccjihhskfzijlb.supabase.co'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVoZHhjZWNjamloaHNrZnppamxiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxNDQ3NDEsImV4cCI6MjA3MTcyMDc0MX0.0IedEaesL7hr9BdRgKENvBNQ9XzTIJ0isfksRU_3nvc'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://ullghcrmleaaualynomj.supabase.co'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsbGdoY3JtbGVhYXVhbHlub21qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxNzA3OTEsImV4cCI6MjA3Mjc0Njc5MX0.gw-uw0WuqsK1JydwyoNGgufWgmj7SGuc62l9zU-RJ9g'
 
 // Validate environment variables
 if (!supabaseUrl || !supabaseAnonKey) {
